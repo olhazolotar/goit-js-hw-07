e75fb7130a69ed08d944d6c4fdd37684386ff3ed
OlhaZolotar <olchere@gmail.com>
2022-06-13 00:15:25 +0100

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryItemsEl = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<a class="gallery__item" href=${original}>
+  <img class="gallery__image"
+  src=${preview}
+  alt=${description} />
+</a>`,
+  )
+  .join('');
+
+galleryItemsEl.innerHTML = markup;
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
